9d4438fc5843c2c91a4e57b5ee37e481f18814fa
Lapo Meucci <lapomeucci@gmail.com>
2018-10-22 12:30:05 +0200

    Minimo e massimo

---
+++ Es. Git/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] vettore = { 2, 5, 3, 23, 12, 8, 6, 7, 1, 29 };
+            int num_min = vettore[0];
+            int num_max = vettore[0];
+
+            num_max = vettore.Max();
+            num_min = vettore.Min();
+
+            Console.WriteLine("Numero massimo : " + num_max);
+            Console.WriteLine("Numero minimo : " + num_min);
+            Console.ReadKey();
+
+            }
+        }
+    }
+
