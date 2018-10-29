8f9e9f247aa20e8be397e61e9f1df591190eab09
Lapo Meucci <lapomeucci@gmail.com>
2018-10-29 12:48:55 +0100

    Terzo commit

--- Es. Git/Program.cs
+++ Es. Git/Program.cs
@@ -25,7 +25,7 @@ namespace Es.Git
             Array.Sort(vettore);
             foreach (int i in vettore) Console.Write(i + " ");
             Console.ReadKey();
-            //lapo  
+            
             
             }
         }
